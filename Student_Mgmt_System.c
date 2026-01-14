24698c4144e8114c09bc4513c29f89aeed634520
Shirish Tamrakar <154002374+Insipid1@users.noreply.github.com>
2026-01-14 18:23:30 +0545

    Add files via upload

---
+++ Student_Mgmt_System.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct Student_Data
+{
+    /* Student ko major info store garna ra binary search ko lagi */
+    
+};
+
+int main()
+{
+    printf("H E L L O.");
+    return 0;
+}
